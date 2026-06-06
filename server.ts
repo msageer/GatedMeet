3a12ea6011911004f636711c075010dee612d05a
Msageer <39546323+msageer@users.noreply.github.com>
2026-06-06 04:51:44 +0100

    refactor: remove unused path variables from server.ts

--- server.ts
+++ server.ts
@@ -1,13 +1,8 @@
 import express from "express";
 import { createServer as createViteServer } from "vite";
 import path from "path";
-import { fileURLToPath } from "url";
 import nodemailer from "nodemailer";
 import Stripe from "stripe";
-
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // Configure Nodemailer transporter
 const transporter = nodemailer.createTransport({
